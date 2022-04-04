64348c0e2cadb29fb4f427be4e028538a7b79b2f
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-03 23:32:03 -0300

    Project created

---
+++ src/test/java/com/patrickmm12/ecommercesystem/EcommerceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickmm12.ecommercesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
