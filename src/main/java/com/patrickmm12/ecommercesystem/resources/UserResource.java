394b502ec6d29935d27fd81fe38e6ab39fcffd98
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-04 23:16:36 -0300

    User entity and resource

---
+++ src/main/java/com/patrickmm12/ecommercesystem/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.patrickmm12.ecommercesystem.resources;
+
+import com.patrickmm12.ecommercesystem.entities.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
