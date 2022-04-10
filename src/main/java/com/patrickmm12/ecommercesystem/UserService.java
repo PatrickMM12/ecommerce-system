f373760c675e7c05a08fa60af0e761057130ac11
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-10 00:01:22 -0300

    Service layer, component registration

---
+++ src/main/java/com/patrickmm12/ecommercesystem/UserService.java
@@ -0,0 +1,26 @@
+package com.patrickmm12.ecommercesystem;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.patrickmm12.ecommercesystem.entities.User;
+import com.patrickmm12.ecommercesystem.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired //Spring realiza a injeção de dependência de forma transparente ao programador
+    private UserRepository repository;
+    
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
--- src/main/java/com/patrickmm12/ecommercesystem/resources/UserResource.java
+++ src/main/java/com/patrickmm12/ecommercesystem/resources/UserResource.java
@@ -1,9 +1,14 @@
 package com.patrickmm12.ecommercesystem.resources;
 
+import java.util.List;
+
+import com.patrickmm12.ecommercesystem.UserService;
 import com.patrickmm12.ecommercesystem.entities.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResource {
     
+    @Autowired //Spring realiza a injeção de dependência de forma transparente ao programador
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){ //@PathVariable = Spring aceitar Id como caminho na URL
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
