d723c50c455bac5c46c689665168350bb821bb15
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-19 22:40:41 -0300

    Payment, one-to-one association

--- src/main/java/com/patrickmm12/ecommercesystem/config/TestConfig.java
+++ src/main/java/com/patrickmm12/ecommercesystem/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.patrickmm12.ecommercesystem.entities.Category;
 import com.patrickmm12.ecommercesystem.entities.Order;
 import com.patrickmm12.ecommercesystem.entities.OrderItem;
+import com.patrickmm12.ecommercesystem.entities.Payment;
 import com.patrickmm12.ecommercesystem.entities.Product;
 import com.patrickmm12.ecommercesystem.entities.User;
 import com.patrickmm12.ecommercesystem.entities.enums.OrderStatus;
@@ -80,5 +81,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
