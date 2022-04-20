25235f567fb524a797c8e277c13441c9a34dfee1
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-19 23:26:28 -0300

    User update

--- src/main/java/com/patrickmm12/ecommercesystem/services/UserService.java
+++ src/main/java/com/patrickmm12/ecommercesystem/services/UserService.java
@@ -12,23 +12,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    @Autowired //Spring realiza a injeção de dependência de forma transparente ao programador
+    @Autowired // Spring realiza a injeção de dependência de forma transparente ao programador
     private UserRepository repository;
-    
-    public List<User> findAll(){
+
+    public List<User> findAll() {
         return repository.findAll();
     }
 
-    public User findById(Long id){
+    public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
 
     public User insert(User obj) {
-		return repository.save(obj);
-	}
+        return repository.save(obj);
+    }
 
     public void delete(Long id) {
         repository.deleteById(id);
     }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getOne(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
