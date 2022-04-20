c1fa99df459b8bede9052d875ee1498767e91ba1
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-19 23:10:29 -0300

    User insert

--- src/main/java/com/patrickmm12/ecommercesystem/entities/User.java
+++ src/main/java/com/patrickmm12/ecommercesystem/entities/User.java
@@ -44,11 +44,11 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public Long getUserId() {
+    public Long getId() {
         return id;
     }
 
-    public void setUserId(Long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
